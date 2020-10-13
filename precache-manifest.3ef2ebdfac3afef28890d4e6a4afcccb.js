60a51eebd86c2ba282b2fa95feb0dc83e65f3054
saad khan <saadgamer124@gmail.com>
2020-10-13 22:10:07 +0530

    Updates

---
+++ precache-manifest.3ef2ebdfac3afef28890d4e6a4afcccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae80383bdffbed466b1287673baf684d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f49379272113111ff0b0",
+    "url": "/monsters-rolodex/static/css/main.9d8c0476.chunk.css"
+  },
+  {
+    "revision": "3844e9a5a0a935e27655",
+    "url": "/monsters-rolodex/static/js/2.b4e2575d.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters-rolodex/static/js/2.b4e2575d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49379272113111ff0b0",
+    "url": "/monsters-rolodex/static/js/main.e83d3a8b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
